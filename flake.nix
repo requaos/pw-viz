e771b05e840da1d07aa495ddb37a9057222c2e82
Neil Skinner <req@requaos.com>
2023-11-21 16:19:52 -0500

    nix flake work by https://github.com/olafklingt/pw-viz-flaked/tree/nix-cargo-integration

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "pw-viz flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    nci.url = "github:yusdacra/nix-cargo-integration";
+    nci.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    nci,
+    ...
+  }:
+    nci.lib.makeOutputs {
+      root = ./.;
+      overrides = {
+        common = prev: {
+          env =
+            prev.env
+            // {
+              LIBCLANG_PATH = prev.pkgs.lib.makeLibraryPath [prev.pkgs.libclang.lib];
+            };
+          buildInputs =
+            (prev.buildInputs or [])
+            ++ [
+              prev.pkgs.libGL
+              prev.pkgs.xorg.libX11
+              prev.pkgs.xorg.libXcursor
+              prev.pkgs.xorg.libXrandr
+              prev.pkgs.xorg.libXi
+              prev.pkgs.xorg.libxcb
+              prev.pkgs.pipewire
+            ];
+          nativeBuildInputs =
+            (prev.nativeBuildInputs or [])
+            ++ [
+              (prev.pkgs.hiPrio prev.pkgs.clang) # needs priority in nix develop shell
+              prev.pkgs.pkg-config
+            ];
+        };
+        packageMetadata = prev: {
+          runtimeLibs =
+            (prev.runtimeLibs or [])
+            ++ [
+              "libGL"
+              "xorg.libxcb"
+              "pkgs.pipewire"
+            ];
+        };
+      };
+    };
+}
